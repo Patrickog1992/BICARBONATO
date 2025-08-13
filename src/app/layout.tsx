f2f5227a55895b2196b97c7d123f9439ed60b093
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-08-13 15:55:09 +0000

    na pagina casamentodosonhos troque o pixel por esse
    <!-- Meta Pixel Co

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,13 +35,13 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1787983728594723');
+            fbq('init', '2263131514090201');
             fbq('track', 'PageView');
           `}
         </Script>
         <noscript>
           <img height="1" width="1" style={{display: 'none'}}
-            src="https://www.facebook.com/tr?id=1787983728594723&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=2263131514090201&ev=PageView&noscript=1"
           />
         </noscript>
       </body>
